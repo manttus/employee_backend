daec39ed7a861e26aa25e5337d0b1e688b67b947
Raymon <basnetraymonn@gmail.com>
2023-01-25 21:55:40 +0545

    add employee route & controller

--- app.ts
+++ app.ts
@@ -2,12 +2,15 @@ import express from "express";
 import dotenv from "dotenv";
 import mongoose from "mongoose";
 import adminRouter from "./routes/admin";
+import cors from "cors";
 
 dotenv.config();
 
 const app = express();
 const PORT = process.env.port || 3000;
 app.use(express.json());
+app.use(cors());
+
 /* The strictQuery option, when set to true, makes Mongoose return an error if you try to query a field that doesn't exist in the model's schema. 
 When set to false, Mongoose will return an empty result instead of an error. */
 
